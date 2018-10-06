52df42b91e08e7be59e9d32feb9294b0fc5c3717
Dani <daniele.minatto@gmail.com>
2018-10-06 19:51:27 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginUserComponent } from './login-user/login-user.component';
+import { LoginCustomerComponent } from './login-customer/login-customer.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'index', component: IndexComponent},
+  {path: '', redirectTo:'/index', pathMatch: 'full' },
+  {path: 'intranet/login', component: LoginUserComponent},
+  {path: 'login', component: LoginCustomerComponent},
+  {path: 'error-page', component: ErrorPageComponent},
+  {path: 'em-construcao', component: UnderConstructionComponent},
+  {path: 'intranet', component: DashboardComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginUserComponent } from './login-user/login-user.component';
+import { LoginCustomerComponent } from './login-customer/login-customer.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    DashboardComponent,
+    LoginUserComponent,
+    LoginCustomerComponent,
+    ErrorPageComponent,
+    UnderConstructionComponent
   ],
   imports: [
     BrowserModule,
